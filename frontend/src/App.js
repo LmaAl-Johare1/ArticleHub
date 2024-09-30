64dadbfafa04f180239a2ce52a3cd48ceeb98f4d
Lama Aref AL Johare <114527203+LmaAl-Johare1@users.noreply.github.com>
2024-09-30 23:30:59 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import AuthPage from './pages/AuthPage/AuthPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
+export default App;
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import ArticlePage from './pages/ArticleDetailPage/ArticleDetailsPage'
 
 function App() {
   return (
-    <AuthePage/>
+    
+    <Router>
+    <Routes>
+         <AuthePage/>
+      {/* Route for HomePage */}
+      <Route path="/" element={<HomePage />} />
+
+      {/* Route for Featured Article Page */}
+      <Route path="/article/featured" element={<ArticlePage />} />
+
+      {/* Route for other articles */}
+      <Route path="/article/:id" element={<ArticlePage />} />
+
+    </Routes>
+  </Router>
   );
 }
 
